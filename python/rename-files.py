d0fb81d0a7560c510eb04d1ed516d26eeb33392e
雷婷婷 <leitingting08@gmail.com>
2017-05-03 20:56:59 +0800

    Update rename-files.py

--- python/rename-files.py
+++ python/rename-files.py
@@ -1,14 +1,17 @@
 import os
 def rename_files():
     #(1) get file names from a folder
-    file_list = os.listdir(r"C:\Users\Administrator\Desktop\study\udacity\python\第二个\prank")
+    file_list = os.listdir(r"C:\Users\Administrator\Desktop\s\udacity\python\第二个\prank")
     #print(file_list)
     saved_path = os.getcwd()
     print("Current Working Directory is "+saved_path)
-    os.chdir(r"C:\Users\Administrator\Desktop\study\udacity\python\第二个\prank")
+    os.chdir(r"C:\Users\Administrator\Desktop\s\udacity\python\第二个\prank")
     #(2) for each file, rename filename
     for file_name in file_list:
+        print("Old Name - "+file_name)
+        print("New Name - "+file_name.translate(None, "0123456789"))
         os.rename(file_name, file_name.translate(None, "0123456789"))
     os.chdir(saved_path)
         
 rename_files()
+
