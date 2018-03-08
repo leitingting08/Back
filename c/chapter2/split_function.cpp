99aa9f8447785b6ca412c6c66fc8d13d12064556
leitingting08 <leitingting08@gmail.com>
2018-03-08 22:32:47 +0800

    chapter2

---
+++ c/chapter2/split_function.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void){
+	double x,y;
+	
+	printf("Enter x:");
+	scanf("%f",x);
+	if(x!=0){
+		y=1/x;
+	}
+	else{
+		y=0;
+	}
+	printf("f(%.2f)=%.1f\n",x,y);
+	return 0;
+} 
