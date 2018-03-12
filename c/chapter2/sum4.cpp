6f9ec33278b8270fc836d3355afb7d02912bd1f4
leitingting08 <leitingting08@gmail.com>
2018-03-12 22:22:55 +0800

    chapter2

---
+++ c/chapter2/sum4.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+int main(void){
+	int k=1;
+	double n,i,sum;
+	printf("Enter n: ");
+	scanf("%lf",&n);
+	sum = 0;
+	for(i=1;i<=3*n;i=i+3){
+		sum = sum + 1/i*k;
+		k=-k;
+	}
+	printf("sum=%.3f",sum);
+	return 0;
+	
+} 
