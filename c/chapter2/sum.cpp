2f05fc12cfc7fb0f7f2a8530ebae1473858ec731
leitingting08 <leitingting08@gmail.com>
2018-03-12 19:25:03 +0800

    chapter

---
+++ c/chapter2/sum.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+	int m,i,sum;
+	printf("Enter: ");
+	scanf("%d",&m);
+	sum = 0;
+	for(i=m;i<=100;i++){
+		sum = sum + i;
+	}
+	printf("sum=%d",sum);
+	return 0;
+	
+} 
