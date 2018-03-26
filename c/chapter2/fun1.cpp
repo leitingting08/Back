87b9a21c21466fd66905c9aa035ecb508853499e
leitingting08 <leitingting08@gmail.com>
2018-03-26 08:28:56 +0800

    chapter2

---
+++ c/chapter2/fun1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int is( int number );
+int main(void){
+	int count,i,sum;
+	count = 0;
+	sum = 0;
+	for(i=100;i<=999;i++)
+	if(is(i) = 1){
+		count++;
+		sum = sum + i; 
+	}
+	printf("count=%d,sum=%d\n",count,sums);
+	return 0;
+}
