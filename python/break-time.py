ae7405d17db5b4ba813adfc3cc95373f54a5b0f4
leitingting08 <leitingting08@gmail.com>
2018-05-25 14:27:27 +0800

    python

--- python/break-time.py
+++ python/break-time.py
@@ -4,8 +4,8 @@ import webbrowser
 total_breaks = 3
 break_count = 0
 
-print("this program started on "+time.ctime())
+print("this program started on "+ time.ctime())
 while(break_count < total_breaks):
-    time.sleep(20*60)
+    time.sleep(20*60) # 间隔20分钟打开项目这个网站
     webbrowser.open("http://www.kugou.com/song/2e5h2a9.html?from_sogou")
     break_count = break_count + 1
