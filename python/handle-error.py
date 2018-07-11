bb3ae616ba739786f8da8ffd63ec02662b83f37a
leitingting08 <leitingting08@gmail.com>
2018-07-11 09:19:44 +0800

    handle_err

--- python/handle-error.py
+++ python/handle-error.py
@@ -1,13 +1,17 @@
 def create_groups(items, num_groups):
-	size = len(items)
+	try:
+		size = len(items)
 
-	groups = []
-	for i in range(0, len(items), size):
-		groups.append(items[i:i + size])
-
-	print("{} groups returned.".format(num_groups))
-	return groups
-
+	except ZeroDivisionError:
+		print("warning:returning empty list. please use a nonzero number.")
+		return []
+	else:
+		groups = []
+		for i in range(0, len(items), size):
+			groups.append(items[i:i + size])
+		return groups
+	finally:
+		print("{} groups returned.".format(num_groups))
 print("creating 6 groups...")
 for group in create_groups(range(32), 6):
 	print(list(group))
